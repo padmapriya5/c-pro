6c7611c648fab56412c4175c5c26c309ba367cea
padmapriya5 <30549887+padmapriya5@users.noreply.github.com>
2018-01-24 16:45:20 +0530

    multi.c

---
+++ multi.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int n,i;
+printf("enter the number "); 
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+        {
+        printf("\n 5 * %ld = %ld",i,5*i);
+        }
+getch();
+}
+
