ef53c5a0dc823414c06715f872276a79fdd0a746
padmapriya5 <30549887+padmapriya5@users.noreply.github.com>
2018-02-05 16:42:45 +0530

    hello.c

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int wordcount;
+printf("hello world");
+scanf("%d",& hello world);
+if(ch==' '||ch=='\n'
+{
+++wordcount;
+}
+printf("words:%d\n",wordcount);
+return 0;
+}
