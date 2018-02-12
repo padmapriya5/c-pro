70ccdacb2fe5b2e40f6f2c7099545c0eba41ca7d
padmapriya5 <30549887+padmapriya5@users.noreply.github.com>
2018-02-12 16:51:50 +0530

    world.c

---
+++ world.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    char s1[100], s2[100], i, j;
+    printf("hello ");
+    scanf("%s", s1);
+    printf("world ");
+    scanf("%s", s2);
+    for(i = 0; s1[i] != '\0'; ++i);
+    for(j = 0; s2[j] != '\0'; ++j, ++i)
+    {
+        s1[i] = s2[j];
+    }
+    s1[i] = '\0';
+    printf("After concatenation: %s", s1);
+    return 0;
