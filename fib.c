ded91581e454888cf92ccd614e2ce83f5faec366
padmapriya5 <30549887+padmapriya5@users.noreply.github.com>
2018-02-19 16:12:12 +0530

    fib.c

---
+++ fib.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int a = 0, b= 1, nextTerm = 0, n;
+    printf("Enter a positive number: ");
+    scanf("%d", &n);
+    printf("Fibonacci Series: %d, %d, ", a, b);
+    nextTerm = a + b;
+    while(nextTerm <= n)
+    {
+        printf("%d, ",nextTerm);
+        a = b;
+        a = nextTerm;
+        nextTerm = a + b;
+    }
+    return 0;
+}
