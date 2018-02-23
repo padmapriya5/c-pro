991936e75ab85a233bbbdcf2b21b899fb835be12
padmapriya5 <30549887+padmapriya5@users.noreply.github.com>
2018-02-23 16:42:30 +0530

    eno.c

---
+++ eno.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+int number;
+    printf("Enter the number: ");
+    scanf("%d", &number);
+    if(number % 2 == 0)
+    {
+        printf("%d is even.");
+    else
+    {
+        printf("%d is odd.");
+        }
+        }
+    return 0;
+    }
