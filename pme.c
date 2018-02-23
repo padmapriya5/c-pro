ec9c4c136e2e11726da8ec1fc8310531ca9351c8
padmapriya5 <30549887+padmapriya5@users.noreply.github.com>
2018-02-23 16:12:25 +0530

    pme.c

---
+++ pme.c
@@ -0,0 +1,22 @@
+#incude<stdio.h>
+int main()
+{
+int count=0,n,i;
+printf("enter the number");
+for(i=2;i<=(n\2);i++)
+{
+if(n%i==0)
+{
+count++;
+}
+}
+if(count==0)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+return 0;
+}
